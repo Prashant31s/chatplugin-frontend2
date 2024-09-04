91a3d44fdfcdcf6e2d2d641cf7708cc048b86659
Prashant31s <pra.shakya00@gmail.com>
2024-09-04 17:10:48 +0530

    changes

--- app/page.js
+++ app/page.js
@@ -2,14 +2,19 @@
 'use client'
 import React from 'react';
 import ChatWindow from './components/ChatWindow';
-
+import { useRouter,useSearchParams  } from 'next/navigation';
 
 const Home = () => {
-  const appId = "your-app-id";  // Replace with dynamic appId as needed
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const appId = searchParams.get("appId")|| "default-app-id";
+  console.log("appId",appId); 
+  // const appId = router.query.appId || "default-app-id";// Replace with dynamic appId as needed
 
   return (
     <div>
-      <h1>Welcome to Chat Plugin</h1>
+      <h1>Welcome to Chat Plugin local {appId}</h1>
+
       <ChatWindow appId={appId} />
     </div>
   );
