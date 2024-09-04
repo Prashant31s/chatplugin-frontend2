ebc1dc5187f35142bbdc5484588a938b68852cb1
Prashant shakya <109499944+Prashant31s@users.noreply.github.com>
2024-09-04 17:57:32 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ import ChatWindow from './components/ChatWindow';
 const Home = () => {
   const router = useRouter();
   const [appId, setAppId] = useState("default-app-id");
-
+  const [x,setX]= useState(0)
   useEffect(() => {
     if (router.isReady) {
       const { appId } = router.query;
@@ -15,11 +15,17 @@ const Home = () => {
     }
   }, [router.isReady, router.query]);
 
+  useEffect(()=>{
+    if(AppId!="default-app-id"){
+      setX(2);
+    }
+  },[AppId,router.isReady, router.quer])
+
   console.log("appId", appId);
 
   return (
     <div>
-      <h1>Welcome to Chat Plugin local {appId}</h1>
+      <h1>Welcome to Chat Plugin local {x}</h1>
       <ChatWindow appId={appId} />
     </div>
   );
