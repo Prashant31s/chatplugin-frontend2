012ed04eac2baa896414ad39852e561bcba53a13
Prashant shakya <109499944+Prashant31s@users.noreply.github.com>
2024-09-10 12:13:01 +0530

    Update ChatWindow.js

--- app/components/ChatWindow.js
+++ app/components/ChatWindow.js
@@ -471,7 +471,7 @@ const chatbox =()=>{
       {
         chatboxopen ?(
           <div className="rounded-2xl  h-screen w-auto p-[2%] ">
-            <div  className="flex flex-row h-[5%] bg-primary rounded-t-lg">
+            <div  className="flex flex-row h-[5%] bg-primary rounded-t-lg py-[1%]">
               <div className="w-[90%] h-[100%]"></div>
               <button onClick={chatbox} className=" w-[10%] h-[100%] "><img src="https://www.svgrepo.com/show/80301/cross.svg"  alt = "close icon"  className = "h-[80%] w-[90%]  invert"></img></button>
             </div>
