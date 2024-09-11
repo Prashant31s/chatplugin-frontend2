c8632a4f9ba377e92337ffa2e1eaae506e1c45ca
Prashant shakya <109499944+Prashant31s@users.noreply.github.com>
2024-09-11 11:31:38 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,11 +14,15 @@ module.exports = {
     'text2':'#FFFFFF',
     'background': '#faf6f8',
     'primary': '#002244',
-    'secondary': '#D0D0D0',
+    'secondary': '#E0E0E0',
     'accent': '#002244',
     'heading':'#000000',
     'joinbutton':'#87CEFA',
-    'joinbutton2':'#002D62'
+    'joinbutton2':'#002D62',
+    'sender':'#F0F0F0',
+    'senderhover':'#A9A9A9',
+    'receiver':'#E0E0E0',
+    'receiverhover':'#BEBEBE'
      },
     extend: {},
   },
